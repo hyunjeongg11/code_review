1c5feb03b14e35aee9da51f74ade532a0a96d045
hyunjeongg11 <guswjd4585@gmail.com>
2024-02-07 17:24:32 +0900

    2021 222024 £2¼차 - 조현정

--- 202402/2주차/반복문자 지우기/반복문자_지우기_조현정.py
+++ 202402/2주차/반복문자 지우기/반복문자_지우기_조현정.py
@@ -3,18 +3,12 @@ sys.stdin = open('input.txt')
 
 T = int(input())
 
-def repeat(string, stack):
-    stack.append(string[0])
+for test in range(1, T+1):
+    string = input()
+    stack = [string[0]]
     for i in range(1, len(string)):
         if stack and stack[-1] == string[i]:
             stack.pop()
         else:
             stack.append(string[i])
-    return len(stack)
-
-
-for test in range(1, T+1):
-    string = list(input())
-    stack = []
-    ans = repeat(string, stack)
-    print(f'#{test} {ans}')
+    print(f'#{test} {len(stack)}')
